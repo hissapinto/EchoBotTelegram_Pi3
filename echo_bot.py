ee2ddf00def82456b487605fc870392782a347a0
hissapinto <hissapinto@hotmail.com>
2026-07-26 19:59:18 -0300

    feat: facts function via API now working

--- echo_bot.py
+++ echo_bot.py
@@ -5,6 +5,8 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from dotenv import load_dotenv
 import logging
 import os
+import requests
+
 
 # Fetch Token via env
 load_dotenv()
@@ -23,7 +25,7 @@ async def start(update: Update, context):
 	"""Sends a welcomer message when the command /start is issued."""
 	user = update.effective_user
 	await update.message.reply_html(
-		f"Saudações {user.mention_html()}! Este que vos fala se entitula como um androide de interlocução pleonástica! Conte-me algo ou digite /help para ajuda...",
+		f"Saudações, {user.mention_html()}! Este que vos fala se entitula como um andróide de interlocução pleonástica! Conte-me algo ou digite /help para ajuda...",
 		# reply_markup=ForceReply(selective=True),
 		# # Optional: force reply to this message
 	)
@@ -37,7 +39,20 @@ async def echo(update: Update, context):
 # help
 async def help(update: Update, context):
 	"""Send a help message."""
-	await update.message.reply_text("Diga-me quaisquer pensamento, seja este frívolo ou pujante, e eu ecoá-lo-ei!")
+	await update.message.reply_text("Diga-me quaisquer pensamento, seja este frívolo ou pujante, e eu ecoá-lo-ei! Se não souber o que espressar, me peça /facts e aprenda algo novo em inglês.")
+# facts
+async def get_fact(update: Update, context):
+	"""Fetch a random fact from an external API."""
+	try:
+		resp = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random")
+		resp.raise_for_status() # Raise an HTTPError for bad responses (4xx or 5xx)
+		fact_data = resp.json()
+		await update.message.reply_text(f"Did you know? {fact_data['text']}")
+	except requests.exceptions.RequestException as e:
+		logger.error(f"Erro ao buscar um fato: {e}")
+		await update.message.reply_text("Deculpe, eu não consegui buscar um fato agora. Tente novamente mais tarde.")
+
+
 
 # main
 def main() -> None:
@@ -52,6 +67,7 @@ def main() -> None:
 	# Register handlers
 	app.add_handler(CommandHandler("start", start))
 	app.add_handler(CommandHandler("help", help))
+	app.add_handler(CommandHandler("facts", get_fact))
 	app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 	# Run the bot until user presses Ctrl-C
