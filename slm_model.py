fbd1ad64999a965267c934e5aee65d93962c717a
hissapinto <isabelahissap@gmail.com>
2026-08-08 12:34:30 -0300

    feat: keep ollama running for 30 min

--- slm_model.py
+++ slm_model.py
@@ -1,12 +1,11 @@
 # meu_gemma.py
-import subprocess
-import time
+
 from ollama import chat
 
 def model_response(input, cabecalho):
 
     try:
-        resposta = chat(
+        response = chat(
             model='gemma3:270m',
             messages=[
                 {
@@ -17,8 +16,9 @@ def model_response(input, cabecalho):
                     'role': 'user',
                     'content': input,
                 },
-            ]
+            ],
+            keep_alive= "30m" # para manter a IA na RAM por 30 min (se não ela é desalocada da RAM depois de 5 min). -1 para ficar sempre ligada
         )
-        return resposta['message']['content']
+        return response['message']['content']
     except Exception as e:
         return f"Erro ao conectar com o Ollama: {e}"
