36f658233b2bb3b59865365168e2ce37c52473f7
hissapinto <isabelahissap@gmail.com>
2026-08-08 00:31:39 -0300

    fix: ollama not subrruing, but om systemd

--- slm_model.py
+++ slm_model.py
@@ -3,15 +3,6 @@ import subprocess
 import time
 from ollama import chat
 
-def start_ollama():
-    """Inicia o Ollama em segundo plano se ele não estiver rodando."""
-    try:
-        chat(model='gemma3:270m', messages=[{'role': 'user', 'content': 'ping'}])
-    except Exception:
-        print("Ollama não está rodando. Iniciando servidor local...")
-        subprocess.Popen(["ollama", "serve"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        time.sleep(5)
-
 def model_response(input, cabecalho):
 
     try:
